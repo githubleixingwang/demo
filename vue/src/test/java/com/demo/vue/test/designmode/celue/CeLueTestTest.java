a80d00238b4b78a9852b30e1d9ba3162c4c6ab52
githubleixingwang <846486188@qq.com>
2022-12-30 18:16:36 +0800

    解析规则-部分代码

--- vue/src/test/java/com/demo/vue/test/designmode/celue/CeLueTestTest.java
+++ vue/src/test/java/com/demo/vue/test/designmode/celue/CeLueTestTest.java
@@ -1,5 +1,6 @@
-package com.demo.vue.test.designpattern.celue;
+package com.demo.vue.test.designmode.celue;
 
+import com.demo.vue.test.designmode.celue.CeLueTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
