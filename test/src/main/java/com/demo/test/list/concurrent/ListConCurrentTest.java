c4f1e9a5936e00007cc4424ac37bd50207dbc8df
githubleixingwang <846486188@qq.com>
2024-03-22 11:15:01 +0800

    list并发写入测试方案优化

--- test/src/main/java/com/demo/test/list/concurrent/ListConCurrentTest.java
+++ test/src/main/java/com/demo/test/list/concurrent/ListConCurrentTest.java
@@ -1,34 +1,29 @@
 package com.demo.test.list.concurrent;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
+/**
+ * 测试list的并发线程安全问题
+ */
 public class ListConCurrentTest {
 
     public static void main(String[] args) throws InterruptedException {
         //List<Object> list = new ArrayList<>();
-        //List<Object> list = Collections.synchronizedList(new ArrayList<>());
-        List<Integer> list = new CopyOnWriteArrayList<>();
+        //List<Object> list = Collections.synchronizedList(new ArrayList<>());//线程安全
+        List<Integer> list = new CopyOnWriteArrayList<>();//线程安全，性能较好
         ExecutorService executorService = Executors.newCachedThreadPool();
-        for (int i = 0; i < 50; i++) {
-            int tem = i;
-            executorService.execute(new Runnable() {
-                @Override
-                public void run() {
-                    list.add(tem);
-                    System.out.println(list);
+        for (int i = 0; i < 10; i++) {//模拟10个线程
+            //Thread.sleep(10);//需要模拟延迟，测试线程安全
+            executorService.execute(() -> {
+                for (int j = 0; j < 10; j++) {
+                    list.add(j);//list写操作
                 }
             });
-//            new Thread(()->{
-//                list.add(tem);
-//                System.out.println(list);
-//            }).start();
-
         }
+        executorService.shutdown();//变量方式创建，需要及时关闭
+        System.out.println(list.stream().count() + "," + list);
     }
 }
